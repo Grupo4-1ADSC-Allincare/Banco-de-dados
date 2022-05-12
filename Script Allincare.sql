825222150d4d515e33ca21d44122dd95cbd593ce
Maryanna Silva <maryanna.silva@sptech.school>
2022-05-12 15:54:31 -0300

    Add files via upload
    Modelagem e Script atualizado

--- Script Allincare.sql
+++ Script Allincare.sql
@@ -3,39 +3,45 @@ USE Allincare;
 
 CREATE TABLE Transportadora(
 	idTransportadora INT PRIMARY KEY AUTO_INCREMENT,
-	nomeFantasia VARCHAR(40),
-    razaoSocial VARCHAR(40),
-    nomeRepresentante VARCHAR(40)
+	nomeFantasia VARCHAR(45),
+    razaoSocial VARCHAR(45),
+    nomeRepresentante VARCHAR(45)
 );
 
 CREATE TABLE UsuarioTransp(
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeUsuario VARCHAR(45),
+    cpfUsuario VARCHAR(14) UNIQUE,
+    numCelular VARCHAR(14) UNIQUE,
+    emailUsuario VARCHAR(45) UNIQUE,
+    senhaUsuario VARCHAR(45)
+);
+
+CREATE TABLE CargoUsuario(
+	fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES UsuarioTransp(idUsuario),
     fkTransportadora INT,
     FOREIGN KEY (fkTransportadora) REFERENCES Transportadora(idTransportadora),
-    nomeUsuario VARCHAR(40),
-    cargoUsuario VARCHAR(13),
-    CHECK (cargoUsuario = 'Administrador' OR cargoUsuario = 'Funcionário'),
-    cpfUsuario VARCHAR(14) UNIQUE,
-    numCelular VARCHAR(11) UNIQUE,
-    emailUsuario VARCHAR(50) UNIQUE,
-    senhaUsuario VARCHAR(20)
+    nomeCargo VARCHAR(11),
+    CHECK (nomeCargo = 'Coordenador' OR nomeCargo = 'Supervisor' OR nomeCargo = 'Estagiário'),
+    cargaHoraria TIME
 );
 
 CREATE TABLE EnderecoTransp(
 	idEnd INT PRIMARY KEY AUTO_INCREMENT,
     fkTransportadora INT,
     FOREIGN KEY (fkTransportadora) REFERENCES Transportadora(idTransportadora),
-    cepEnd VARCHAR(10),
+    cepEnd VARCHAR(9),
     numEnd INT,
-    complementoEnd VARCHAR(20),
-    cnpjTransportadora VARCHAR(20) UNIQUE
+    complementoEnd VARCHAR(45),
+    cnpjTransportadora VARCHAR(18) UNIQUE
 );
 
 CREATE TABLE ContatoTransp(
 	idContato INT PRIMARY KEY AUTO_INCREMENT,
     fkTransportadora INT,
     FOREIGN KEY (fkTransportadora) REFERENCES Transportadora(idTransportadora),
-    numeroContato VARCHAR(11) UNIQUE,
+    numeroContato VARCHAR(14) UNIQUE,
     tipoNum VARCHAR(9),
     CHECK (tipoNum = 'Fixo' OR tipoNum = 'Celular' OR tipoNum = 'Whatsapp' OR tipoNum = 'Comercial')
 ); 
@@ -46,57 +52,63 @@ CREATE TABLE Veiculo(
     FOREIGN KEY (fkTransportadora) REFERENCES Transportadora(idTransportadora),
 	gps VARCHAR(3),
     CHECK (gps = 'ON' OR gps = 'OFF'),
-	placa VARCHAR(10) UNIQUE,
+	placa VARCHAR(6) UNIQUE,
 	qtdLotesVacina INT,
-    qtdDispositivos INT,
-	modelo VARCHAR(20)
+	modelo VARCHAR(45)
 );
 
 CREATE TABLE Sensor(
 	idSensor INT PRIMARY KEY AUTO_INCREMENT,
-	fkVeiculo INT,
+    fkVeiculo INT,
     FOREIGN KEY (fkVeiculo) REFERENCES Veiculo(idVeiculo),
-	nomeDispositivo VARCHAR(20),
-    localidade VARCHAR(20),
+    nomeSensor VARCHAR(45),
+    tipoSensor VARCHAR(45),
 	situacao VARCHAR(3),
-    CHECK (situacao = "ON" OR situacao = "OFF"),
-    tipoSensor VARCHAR(10),
-    nomeSensor VARCHAR(20)
+    CHECK (situacao = "ON" OR situacao = "OFF")
 );
 
-CREATE TABLE Medida(
-	idMedida INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE HistMedida(
+	idHistMedida INT PRIMARY KEY AUTO_INCREMENT,
 	fkSensor INT,
     FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor),
-    valorTemperatura DECIMAL,
+    valorTemperatura INT,
 	dataHora DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 SELECT * FROM Transportadora;
 SELECT * FROM UsuarioTransp;
+SELECT * FROM CargoUsuario;
 SELECT * FROM EnderecoTransp;
 SELECT * FROM ContatoTransp;
 SELECT * FROM Veiculo;
 SELECT * FROM Sensor;
-SELECT * FROM Medida;
+SELECT * FROM HistMedida;
 
 INSERT INTO Transportadora
-VALUES (NULL,'FreteBras','FreteBras Internet e Serviços LTDA','11.111.111/0001-11','Larissa Freitas'),
-		(NULL,'Loggi','Loggi Tecnologia LTDA','22.222.222/0001-22','Fabiano Rodrigues'),
-        (NULL,'Lalamove','LALAMOVE TECNOLOGIA (BRASIL) LTDA','33.333.333/0001-33','Cecília Almeida');
-        
+VALUES (NULL,'FreteBras','FreteBras Internet e Serviços LTDA','Larissa Freitas'),
+		(NULL,'Loggi','Loggi Tecnologia LTDA','Fabiano Rodrigues'),
+        (NULL,'Lalamove','LALAMOVE TECNOLOGIA (BRASIL) LTDA','Cecília Almeida');
+
 INSERT INTO UsuarioTransp
-VALUES (NULL,'1','Edson Feitoza','Administrador','111.111.111-11','11982993672','edson.feitoza@gmail.com','123edson'),
-		(NULL,'1','Guilherme Tarifa','Funcionário','111.111.111-00','11982133672','guilherme.tarifa@gmail.com','123guilherme'),
-		(NULL,'2','Paula Cristina','Administrador','222.222.222-22','11953660154','paula.cristina@gmail.com','123paula'),
-        (NULL,'2','Kauan Mendes','Funcionário','222.222.222-00','11953987154','kauan.mendes@gmail.com','123kauan'),
-        (NULL,'3','Alice Santos','Administrador','333.333.333-33','11980213320','alice.santos@gmail.com','123alice'),
-        (NULL,'3','Emilly Dantas','Funcionário','333.333.333-00','11980998320','emilly.dantas@gmail.com','123emilly');
+VALUES (NULL,'Edson Feitoza','111.111.111-11','11982993672','edson.f@gmail.com','123edson'),
+		(NULL,'Guilherme Tarifa','111.111.111-00','11982133672','guilherme.t@gmail.com','123guilherme'),
+		(NULL,'Paula Cristina','222.222.222-22','11953660154','paula.c@gmail.com','123paula'),
+        (NULL,'Kauan Mendes','222.222.222-00','11953987154','kauan.m@gmail.com','123kauan'),
+        (NULL,'Alice Santos','333.333.333-33','11980213320','alice.s@gmail.com','123alice'),
+        (NULL,'Emilly Dantas','333.333.333-00','11980998320','emilly.d@gmail.com','123emilly');
+        
+INSERT INTO CargoUsuario
+VALUES ('1','2','Coordenador','08:00:00'),
+		('2','3','Supervisor','10:00:00'),
+        ('3','1','Estagiário','06:00:00'),
+        ('4','2','Coordenador','08:00:00'),
+        ('5','3','Supervisor','10:00:00'),
+        ('6','1','Estagiário','06:00:00');
         
 INSERT INTO EnderecoTransp
-VALUES (NULL,'1','89872-000','123','12º Andar'),
-		(NULL,'2','89843-000','87','Bloco 5'),
-        (NULL,'3','89859-000','3245','3º Andar');
+VALUES (NULL,'1','89872-000','123','12º Andar','00000000000'),
+		(NULL,'2','89843-000','87','Bloco 5','11111111111'),
+        (NULL,'3','89859-000','3245','3º Andar','22222222222');
         
 INSERT INTO ContatoTransp
 VALUES (NULL,'1','11954678342','Comercial'),
@@ -106,25 +118,33 @@ VALUES (NULL,'1','11954678342','Comercial'),
         (NULL,'3','11922278342','Whatsapp');
         
 INSERT INTO Veiculo
-VALUES (NULL,'1','ON','QPR3C77','50','2','VUC'),
-		(NULL,'1','ON','ASD4G56','100','2','Caminhão ¾'),
-        (NULL,'2','ON','LKJ1O98','150','2','Toco'),
-        (NULL,'2','ON','BGC5H90','200','2','Truck'),
-        (NULL,'3','ON','OKJ7L43','250','2','Carreta'),
-        (NULL,'3','ON','UHJ9M32','300','2','Caminhão combinado');
-        
+VALUES (NULL,'1','ON','KRF395','2','Volks'),
+		(NULL,'1','ON','HOA639','2','Volks'),
+		(NULL,'1','ON','PHF245','2','Mercedes'),
+		(NULL,'2','ON','JÇS846','2','Mercedes'),
+		(NULL,'2','ON','IHF465','2','Volks'),
+		(NULL,'2','ON','XCH824','2','Volvo'),
+        (NULL,'3','ON','PHF321','2','Mercedes'),
+		(NULL,'3','ON','JDN846','2','Volvo'),
+		(NULL,'3','ON','LKO465','2','Mercedes'),
+		(NULL,'3','ON','XCB634','2','Volvo');
+	
 INSERT INTO Sensor
-VALUES (NULL,'1','Arduíno UNO','Carroceria','ON','LM35','Temperatura'),
-		(NULL,'2','Arduíno UNO','Traseira','ON','LM35','Temperatura'),
-        (NULL,'3','Arduíno UNO','Carroceria','ON','LM35','Temperatura'),
-        (NULL,'4','Arduíno UNO','Traseira','ON','LM35','Temperatura'),
-        (NULL,'5','Arduíno UNO','Carroceria','ON','LM35','Temperatura'),
-        (NULL,'6','Arduíno UNO','Traseira','ON','LM35','Temperatura');
+VALUES (NULL,'1','LM35','Temperatura','ON'),
+		(NULL,'2','LM35','Temperatura','ON'),
+		(NULL,'3','LM35','Temperatura','ON'),
+        (NULL,'4','LM35','Temperatura','ON'),
+        (NULL,'5','LM35','Temperatura','ON'),
+        (NULL,'6','LM35','Temperatura','ON'),
+        (NULL,'7','LM35','Temperatura','ON'),
+        (NULL,'8','LM35','Temperatura','ON'),
+        (NULL,'9','LM35','Temperatura','ON'),
+        (NULL,'10','LM35','Temperatura','ON');
         
-INSERT INTO Medida
-VALUES (NULL,'1','32°',CURRENT_TIMESTAMP),
-		(NULL,'1','3°',CURRENT_TIMESTAMP),
-        (NULL,'2','12°',CURRENT_TIMESTAMP),
-        (NULL,'2','40°',CURRENT_TIMESTAMP),
-        (NULL,'3','7°',CURRENT_TIMESTAMP),
-        (NULL,'3','15°',CURRENT_TIMESTAMP);
+INSERT INTO HistMedida
+VALUES (NULL,'1','32',CURRENT_TIMESTAMP),
+		(NULL,'1','3',CURRENT_TIMESTAMP),
+        (NULL,'2','12',CURRENT_TIMESTAMP),
+        (NULL,'2','40',CURRENT_TIMESTAMP),
+        (NULL,'3','7',CURRENT_TIMESTAMP),
+        (NULL,'3','15',CURRENT_TIMESTAMP);
